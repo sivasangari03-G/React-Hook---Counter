a451851f717455a7f6c546844180009a5df81b43
SANGARIGURU <sangariguru03@gmail.com>
2022-02-03 22:40:52 +0530

    double

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,13 +11,18 @@ function Counter() {
 	const handleDecrement = (value) => {
 		setCounter(counter - value);
 	};
+
+	const handleDouble = (value) => {
+		setCounter(counter * value);
+	};
 	// console.log({ counter });
 	// console.log({handleIncrement});
 	return (
 		<>
 			<h1>{counter}</h1>
-			<button onClick={() => handleIncrement(1)}>INCREASE</button>
-			<button onClick={() => handleDecrement(1)}>DECREASE</button>
+			<button onClick={() => handleIncrement(1)}>Increase</button>
+			<button onClick={() => handleDecrement(1)}>Decrease</button>
+			<button onClick={() => handleDouble(2)}>Double</button>
 		</>
 	);
 }
